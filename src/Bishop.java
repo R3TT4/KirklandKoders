40eeb0ef201623e5e2d76b621215cf5aa2a1dfa5
sarahahagen <shagen@zagmail.gonzaga.edu>
2021-10-16 11:17:25 -0700

    Bishop class added

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Bishop {
+public class Bishop extends Piece {
+
+    // dvc
+    public Bishop() {
+        super("Bishop");
+        setxPos(0);
+        setyPos(0);
+    }
+
+    // evc
+    public Bishop(int xPos, int yPos) {
+        super("Bishop");
+        setxPos(xPos);
+        setyPos(yPos);
+    }
+
+    @Override
+    public boolean validateMove(int currX, int currY, int newY, int newX) {
+        // boolean canPlay = false;
+
+        // checks if play can work
+        if (currX == newX || currY == newY) {
+            return true;
+        }
+        return false;
+    }
 }
