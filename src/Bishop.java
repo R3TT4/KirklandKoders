fccd377b9482e316c6398b17ca2b14f9a6f09a3f
sarahahagen <shagen@zagmail.gonzaga.edu>
2021-10-16 11:54:44 -0700

    Fixed Bishop to only go diagonally

--- src/Bishop.java
+++ src/Bishop.java
@@ -16,12 +16,23 @@ public class Bishop extends Piece {
 
     @Override
     public boolean validateMove(int currX, int currY, int newY, int newX) {
+        // this is for pushing branch to repository
         // boolean canPlay = false;
 
         // checks if play can work
-        if (currX == newX || currY == newY) {
-            return true;
+        // bishop only goes diagonally
+
+        if (Math.abs(currX - 1) == newX) {
+            if (Math.abs(currY - 1) == newY || Math.abs(currY + 1) == newY) {
+                return true;
+            }
         }
+        else if (Math.abs(currX + 1) == newX) {
+            if (Math.abs(currY + 1) == newY || Math.abs(currY - 1) == newY) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
